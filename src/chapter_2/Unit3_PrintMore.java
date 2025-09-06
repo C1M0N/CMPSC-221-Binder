701ffd65ab9d2d4b3a8fad7ae13931a3f58931b6
CiMON文 <onoettl@outlook.com>
2025-09-06 17:28:46 -0400

    Add examples for print and printf usage
    Added Unit3_PrintMore.java to demonstrate the difference between print and println, and Unit4_Printf.java to show usage of printf for formatted output in chapter_2.

---
+++ src/chapter_2/Unit3_PrintMore.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2025 Doucheng Peng
+ *
+ * Author      : Doucheng Peng [C1M0N]
+ * Email       : dbp5625@psu.edu / cimon@cementine.ink
+ * Course      : CMPSC 221 [Autumn 002]
+ * Date        : 9/6/25 PM5:07
+ *
+ * Collaboration Statement: By myself
+ * Description : print和println的区别
+ */
+package chapter_2;
+
+public class Unit3_PrintMore {
+  public static void main(String[] args){
+    System.out.print("Hello\t\t\tHello\r啥都看不见！");
+  }
+}
---
+++ src/chapter_2/Unit4_Printf.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2025 Doucheng Peng
+ *
+ * Author      : Doucheng Peng [C1M0N]
+ * Email       : dbp5625@psu.edu / cimon@cementine.ink
+ * Course      : CMPSC 221 [Autumn 002]
+ * Date        : 9/6/25 PM5:24
+ *
+ * Collaboration Statement: By myself
+ * Description : 终极打印！
+ */
+package chapter_2;
+
+public class Unit4_Printf {
+
+  public static void main(String[] args) {
+    System.out.printf("这是第%s行%n这是第%s行%n", "1", "2");
+  }
+
+}
