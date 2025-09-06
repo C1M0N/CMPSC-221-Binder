ad29f7da52d741aab29d6c93f1174abaeffe7bf7
CiMON文 <onoettl@outlook.com>
2025-09-06 17:05:42 -0400

    Replace HelloWorld.java with Unit2_HelloWorld.java
    Deleted HelloWorld.java and added Unit2_HelloWorld.java with a main method that prints 'Hello World!'. This change introduces a functional Hello World example for chapter 2.

---
+++ src/chapter_2/Unit2_HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2025 Doucheng Peng
+ *
+ * Author      : Doucheng Peng [C1M0N]
+ * Email       : dbp5625@psu.edu / cimon@cementine.ink
+ * Course      : CMPSC 221 [Autumn 002]
+ * Date        : 9/6/25 PM4:53
+ *
+ * Collaboration Statement: By myself
+ * Description : 一个测试
+ */
+package chapter_2;
+
+public class Unit2_HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
