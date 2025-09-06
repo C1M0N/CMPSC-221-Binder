aea018c3fa97b7cdcd933dddebdc57348c5c1f9d
CiMON文 <onoettl@outlook.com>
2025-09-06 16:17:26 -0400

    Add HelloWorld.java and project dictionary
    Created HelloWorld.java in chapter_2 with initial class structure and added .idea project dictionary containing a custom word.

---
+++ src/chapter_2/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+Author : Doucheng Peng
+GitHub : C1M0N
+Course : CMPSC 221 [Autumn Class 002]
+File   : CMPSC-221-Binder/src/chapter_2/HelloWorld.java
+
+Collaboration Statement: By myself
+Date  : 2025/09/06 16:13
+*/
+
+package chapter_2;
+
+public class HelloWorld {
+
+}
